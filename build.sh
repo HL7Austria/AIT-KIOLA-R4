8b14db8d5730c37aa246c62ae62de02a1400b6e1
Lukas Roedl <lukas.roedl@ait.ac.at>
2023-03-15 11:30:20 +0100

    Working on installation and build scripts

--- build.sh
+++ build.sh
@@ -1,1 +1,37 @@
-java -jar ./input-cache/publisher.jar -ig ig.ini
+#!/bin/bash
+
+SD=$(cd "$(dirname "$0")"; pwd -P)
+WD="$(pwd)"
+SCRIPT=$(basename "$0")
+SCRIPT_NAME=${SCRIPT%.*}
+SCRIPT_EXTENSION=${SCRIPT##*.}
+SELF=$SD/$SCRIPT
+
+#/////////////////////////
+
+APP_JAVA="$(which java)"
+
+#/////////////////////////
+
+set -eu
+
+#/////////////////////////
+
+if [ ! -e "$APP_JAVA" ] ; then
+    echo "The executable 'java' wasn't found!"
+    exit
+fi
+
+#/////////////////////////
+
+cd "$SD/"
+
+#/////////////////////////
+
+$APP_JAVA -jar $SD/input-cache/publisher.jar -ig ig.ini
+
+#/////////////////////////
+
+cd "$WD/"
+
+#/////////////////////////
